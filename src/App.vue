d5e9b460b53a7222a1623f408a090b84851065b0
Yaavi <27679365@qq.com>
2021-01-11 12:10:33 +0800

    增加 workflow

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="这是 由 GitHub Action 自动部署的 Gitpage"/>
   </div>
 </template>
 
