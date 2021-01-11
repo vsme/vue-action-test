dd961cc4a521b5aa599a155da82288b71b5b91ff
Yaavi <27679365@qq.com>
2021-01-11 13:59:33 +0800

    纳尼

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <HelloWorld msg="这是 由 GitHub Action 自动部署的 Gitpage"/>
   </div>
 </template>
